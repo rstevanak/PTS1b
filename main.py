9b424efc2698557ad8566dd2bf7fb6574f83dacd
Roman Števaňák <rstevanak@gmail.com>
2017-03-10 22:15:04 +0100

    reformated code into functions

--- main.py
+++ main.py
@@ -3,31 +3,47 @@ import math
 sutaziaci = {}
 juniori = set()
 
+def addpoints(meno, body):
+    body = float(body) + sutaziaci.get(meno, 0)
+    sutaziaci[meno] = body
+
+
+def reducepoints(percento):
+    for hrac in sutaziaci:
+        body = sutaziaci[hrac]
+        inverzne_percento = (1 - (float(percento) / 100))
+        nove_body = math.floor(body * inverzne_percento)
+        sutaziaci[hrac] = nove_body
+
+
+def junior(meno):
+    if meno in sutaziaci:
+        juniori.add(meno)
+
+
+def ranks(jr=None):
+    vypisujem_vsetkych = True
+    if jr and jr == "junior":
+        vypisujem_vsetkych = False
+    usortene = sorted(sutaziaci.items(), key=lambda x: x[1], reverse=True)
+    for hrac, body in usortene:
+        if vypisujem_vsetkych or hrac in juniori:
+            print("Hrac {} ma {:g} bodov".format(hrac, body))
+
+
 while True:
     command = input().split()
     if command[0] == "points":
-        body = sutaziaci.get(command[1], 0) + float(command[2])
-        sutaziaci[command[1]] = body
+        addpoints(*command[1:])
 
     elif command[0] == "reduce":
-        for hrac in sutaziaci:
-            body = sutaziaci[hrac]
-            inverzne_percento = (1 - (float(command[1]) / 100))
-            nove_body = math.floor(body * inverzne_percento)
-            sutaziaci[hrac] = nove_body
+        reducepoints(*command[1:])
 
     elif command[0] == "junior":
-        if command[1] in sutaziaci:
-            juniori.add(command[1])
+        junior(*command[1:])
 
     elif command[0] == "ranking":
-        vypisujem_vsetkych = True
-        if len(command) > 1 and command[1] == "junior":
-            vypisujem_vsetkych = False
-        usortene = sorted(sutaziaci.items(), key=lambda x: x[1], reverse=True)
-        for hrac, body in usortene:
-            if vypisujem_vsetkych or hrac in juniori:
-                print("Hrac {} ma {:g} bodov".format(hrac, body))
+        ranks(*command[1:])
 
     elif command[0] == "quit":
         print("Goodbye")
